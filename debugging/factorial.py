5a8d61ad05bdaf924eea015ea28295b968fa5107
ofest <dars20@hotmail.com>
2025-09-01 13:59:25 +0200

    Add PY factorial.

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,10 +5,12 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1   # 👈 Decrementamos n
+        n -= 1   # decrease n each step
     return result
 
-if __name__ == "__main__":
+if len(sys.argv) > 1:
     f = factorial(int(sys.argv[1]))
     print(f)
+else:
+    print("Usage: ./factorial.py <number>")
     
